1f5ec1dacb34a4e75585599853db6be96eb402c6
Ann <1187484@gmail.com>
2022-04-12 15:03:17 +0300

    game

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 
 kompukter = random.randint(1, 100)
@@ -18,25 +17,4 @@ for i in range(attempt):
 if a == str(kompukter):
     print('Игра окончена!')
 else:
-=======
-import random
-
-kompukter = random.randint(1, 100)
-attempt = 6
-print(kompukter)
-for i in range(attempt):
-    a = input()
-    if a == str(kompukter):
-        print('Угадал!')
-        break
-    elif not a.isnumeric():
-        print('Компуктер загадал число. Подсказка: Введи число!')
-    elif int(a) > kompukter:
-        print('Подсказка:Загаданое число меньше!')
-    elif int(a) < kompukter:
-        print('Подсказка: Загаданое число больше!')
-if a == str(kompukter):
-    print('Игра окончена!')
-else:
->>>>>>> 6a8f7fda44b384ee4dba20cfb2498ce504d601aa
     print('Попытки кончились!')  
