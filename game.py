c3a418c802664cbb598abc1e622c44f0f7e889d7
Ann <1187484@gmail.com>
2022-04-13 15:08:43 +0300

    new

--- game.py
+++ game.py
@@ -1,27 +1,36 @@
-from itertools import count
 import random
 
 kompukter = random.randint(1, 100)
-attempt = 6
-count_attempt = 0
+attempt = 3
+count_attempt = 1
 print(kompukter)
-print('Введите число')
+print('Компуктер загадал число. Введите свое число!')
 a = input()
-while count_attempt < attempt:
-    if not a.isnumeric():
-        print('Компуктер загадал число! Введите Число!')
-        a = input()
+again = None
+while True:
+    while count_attempt < attempt:
+        if not a.isnumeric():
+            print('Вы ввели букву или символ. Введите Число!')
+            a = input()
+        else:
+            count_attempt+=1
+            if a == str(kompukter):
+                print('Вы угадали!')
+                break
+            elif a > str(kompukter):
+                print('Загаданое компуктером число меньше Вашего числа!')
+                print('У Вас осталось попыток:' + str(attempt+1 - count_attempt))
+            elif a < str(kompukter):
+                print('Загаданое компуктером число больше Вашего числа!')
+                print('у Вас осталось попыток:' + str(attempt+1 - count_attempt))
+            a = input()
+    if count_attempt == attempt:
+        print('У вас кончились попытки :( Вы так и не угадали. Хотите сыграть еще раз? (Да/Нет)')
+        again = input().lower()
     else:
-        count_attempt+=1
-        if a == str(kompukter):
-            print('Угадал!')
-            break
-        elif a > str(kompukter):
-            print('Загаданое число больше!')
-        elif a < str(kompukter):
-            print('Загаданое число меньше!')
-        a = input()
-if count_attempt == attempt:
-    print("Попытки кончились")
-else:
-    print('Игра окончена!')
+        print('Игра окончена! Хотите сыграть еще раз? (Да/Нет)')
+        again = input().lower()
+    if again == 'да':
+        True
+    else:
+        False
