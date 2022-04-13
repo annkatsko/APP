5e216113bbe8f415e7b3c18ed81dca845f791dfb
Ann <1187484@gmail.com>
2022-04-13 15:23:20 +0300

    nrw

--- game.py
+++ game.py
@@ -4,33 +4,31 @@ kompukter = random.randint(1, 100)
 attempt = 3
 count_attempt = 1
 print(kompukter)
-print('Компуктер загадал число. Введите свое число!')
-a = input()
+a = input('Компуктер загадал число. Введите свое число!')
 again = None
-while True:
-    while count_attempt < attempt:
-        if not a.isnumeric():
-            print('Вы ввели букву или символ. Введите Число!')
-            a = input()
-        else:
-            count_attempt+=1
-            if a == str(kompukter):
-                print('Вы угадали!')
-                break
-            elif a > str(kompukter):
-                print('Загаданое компуктером число меньше Вашего числа!')
-                print('У Вас осталось попыток:' + str(attempt+1 - count_attempt))
-            elif a < str(kompukter):
-                print('Загаданое компуктером число больше Вашего числа!')
-                print('у Вас осталось попыток:' + str(attempt+1 - count_attempt))
-            a = input()
-    if count_attempt == attempt:
-        print('У вас кончились попытки :( Вы так и не угадали. Хотите сыграть еще раз? (Да/Нет)')
-        again = input().lower()
+
+while count_attempt < attempt:
+    if not a.isnumeric():
+        print('Вы ввели букву или символ. Введите Число!')
+        a = input()
     else:
-        print('Игра окончена! Хотите сыграть еще раз? (Да/Нет)')
-        again = input().lower()
-    if again == 'да':
-        True
-    else:
-        False
+        count_attempt += 1
+        if a == str(kompukter):
+            print('Вы угадали!')
+            break
+        elif a > str(kompukter):
+            print('Загаданое компуктером число меньше Вашего числа!')
+            print('У Вас осталось попыток:' + str(attempt+1 - count_attempt))
+        elif a < str(kompukter):
+            print('Загаданое компуктером число больше Вашего числа!')
+            print('у Вас осталось попыток:' + str(attempt+1 - count_attempt))
+        a = input()
+if count_attempt == attempt:
+    again = input('У вас кончились попытки :( Вы так и не угадали. Хотите сыграть еще раз? (Да/Нет)').lower()
+else:
+    again = input('Игра окончена! Хотите сыграть еще раз? (Да/Нет)').lower()
+if again == 'да':
+    True
+    continue
+else:
+    False
