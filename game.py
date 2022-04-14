92c5c24adb86bf36aab3e0a16983ce50bf4af6ec
Ann <1187484@gmail.com>
2022-04-14 13:30:47 +0300

    new

--- game.py
+++ game.py
@@ -5,8 +5,9 @@ attempt = 3
 count_attempt = 1
 print(kompukter)
 user_number = input('Компуктер загадал число. Введите свое число!')
-raund = None
-while True:
+user_answer = None
+game = True
+while game:
     while count_attempt < attempt:
         if not user_number.isnumeric():
             print('Вы ввели букву или символ. Введите Число!')
@@ -24,21 +25,22 @@ while True:
                 print('у Вас осталось попыток:' + str(attempt+1 - count_attempt))
             user_number = input()
     if count_attempt == attempt:
-        raund = input('У вас кончились попытки :( Вы так и не угадали. Хотите сыграть еще раз? (Да/Нет)').lower()
-        if raund == 'нет':
-            print('Вы вышли из игры!')
-            quit()
-        elif raund == 'да':
-            kompukter = random.randint(1, 100)
-            count_attempt = 1
-            continue
-    
+            user_answer = input('У вас кончились попытки :( Вы так и не угадали. Хотите сыграть еще раз? (Да/Нет)').lower()
+            if user_answer == 'нет':
+                print('Вы вышли из игры')
+                game = False
+            elif user_answer == 'да':
+                kompukter = random.randint(1, 100)
+                count_attempt = 1
+                user_number = input('Играем снова! Компуктер загадал число. Введите свое число!')
+                continue
     else:
-        raund = input('Игра окончена! Хотите сыграть еще раз? (Да/Нет)').lower()
-        if raund == 'нет':
-            print('Вы вышли из игры!')
-            quit()
-        elif raund == 'да':
+        user_answer = input('Хотите сыграть еще раз? (Да/Нет)').lower()
+        if user_answer == 'нет':
+            print('Вы вышли из игры')
+            game = False
+        elif user_answer == 'да':
             kompukter = random.randint(1, 100)
             count_attempt = 1
+            user_number = input('Играем снова! Компуктер загадал число. Введите свое число!')
             continue
