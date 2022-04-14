f6f0b26fba7faef03ce19dcf1b12e08f189ec74e
Ann <1187484@gmail.com>
2022-04-14 12:04:49 +0300

    neww

--- game 2.py
+++ game 2.py
@@ -4,25 +4,25 @@ kompukter = random.randint(1, 100)
 attempt = 3
 count_attempt = 1
 print(kompukter)
-user_number = input('Компуктер загадал число. Введите свое число!')
+a = input('Компуктер загадал число. Введите свое число!')
 raund = None
 while True:
     while count_attempt < attempt:
-        if not user_number.isnumeric():
+        if not a.isnumeric():
             print('Вы ввели букву или символ. Введите Число!')
-            user_number = input()
+            a = input()
         else:
             count_attempt += 1
-            if user_number == str(kompukter):
+            if a == str(kompukter):
                 print('Вы угадали!')
                 break
-            elif user_number > str(kompukter):
+            elif a > str(kompukter):
                 print('Загаданое компуктером число меньше Вашего числа!')
                 print('У Вас осталось попыток:' + str(attempt+1 - count_attempt))
-            elif user_number < str(kompukter):
+            elif a < str(kompukter):
                 print('Загаданое компуктером число больше Вашего числа!')
                 print('у Вас осталось попыток:' + str(attempt+1 - count_attempt))
-            user_number = input()
+            a = input()
     if count_attempt == attempt:
         raund = input('У вас кончились попытки :( Вы так и не угадали. Хотите сыграть еще раз? (Да/Нет)').lower()
         if raund == 'нет':
