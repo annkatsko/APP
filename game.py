f8e79b6a3a2cbefb374aca57425366fce36b28e5
Ann <1187484@gmail.com>
2022-04-15 21:44:31 +0300

    ожд

--- game.py
+++ game.py
@@ -8,10 +8,9 @@ while game:
     attempt = 3
     kompukter = random.randint(1, 100)
     print(kompukter)
-    user_number = input('Компуктер загадал число. Введите свое число!')
-    user_answer = None
     raund = True
     while raund:
+        user_number = input('Компуктер загадал число. Введите свое число!')
         while not user_number.isnumeric():
             print('Вы ввели букву или символ. Введите Число!')
             user_number = input()
@@ -25,15 +24,8 @@ while game:
             print('Загаданое компуктером число больше Вашего числа!')
             print('у Вас осталось попыток:' + str(attempt - count_attempt))
         user_number = input()
-        count_attempt += 1
         if count_attempt == attempt:
             raund = False
-    user_answer = input('У вас кончились попытки :( Вы так и не угадали. Хотите сыграть еще раз? (Да/Нет)').lower()
-    if user_answer == 'нет':
-        print('Вы вышли из игры')
-        game = False
-    elif user_answer == 'да':
-        continue
     user_answer = input('Хотите сыграть еще раз? (Да/Нет)').lower()
     if user_answer == 'нет':
         print('Вы вышли из игры')
