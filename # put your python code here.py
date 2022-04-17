4e1c6e942ada276e74d492e053b4ecc11a866817
annkatsko <1187484@gmail.com>
2022-04-17 07:18:14 +0300

    first commit

---
+++ # put your python code here.py
@@ -0,0 +1,18 @@
+# put your python code here
+def func(email):
+     t = {'A', 'E', 'I', 'O', 'U', 'Y', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S',        'T', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_', 'a', 'e', 'i', 'o', 'u',        'y', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',        '@', '.'}
+
+     for i in email:
+        if i in t:
+            if '@' and '.' in email:
+                print('ДА')
+                break
+            else:
+                print('НЕТ')
+                break
+
+em=input()            
+func(em)
+
+
+
