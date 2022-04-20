f6b5bb0d3affb68e7550218a28596f7b87b919d0
annkatsko <1187484@gmail.com>
2022-04-20 13:14:38 +0300

    updated

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ from tokenize import Number
 def main(number):
     f = {0: 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
     6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine'}
-    return f[number] if 0 <= number < 10 else print('enter a number')
+    #return f[number] if 0 <= number < 10 else print('enter a number')
+    return f.get(number,'there is no such value')
 
-
-print(main(f))
+print(main('k'))
