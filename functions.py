97ebb383b7d1e14bfd7d40f6b714208c706a46a3
annkatsko <1187484@gmail.com>
2022-04-20 09:51:52 +0300

    first version

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
+from tokenize import Number
+
+
 def main(number):
-    f = {0: '"null"', 1 : '"one"', 2 : '"two"', 3 : '"three"', 4 : '"four"', 5 : '"five"',
-    6 : '"six"', 7 : '"seven"', 8 : '"eight"', 9 : '"nine"'}
-    return f[number]
+    f = {0: 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+    6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine'}
+    return f[number] if 0 <= number < 10 else print('enter a number')
 
 
-a = main(5)
-print(a)
+print(main(f))
